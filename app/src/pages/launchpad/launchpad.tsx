d76219f91133fc1cb09bd1dd7c152f5c4342832a
Snuffish <snuffish90@gmail.com>
2023-10-08 02:16:10 +0200

    wip

--- app/src/pages/launchpad/launchpad.tsx
+++ app/src/pages/launchpad/launchpad.tsx
@@ -1,48 +1,25 @@
-import { Grid } from '@material-ui/core'
-import Pad from 'Components/Pad'
-import Slider from 'Components/Slider'
-import Test from 'Components/Test'
-import { CHANNELS } from 'Constants/ipc'
-import React, { createContext } from 'react'
+import { CHANNELS } from "Constants/ipc"
+import React from "react"
+import { createContext } from "react"
 
-const createGrid = () => {
-  const rows = []
-  for (let y = 8; y >= 1; y--) {
-    const pads = []
-    for (let x = 1; x <= 8; x++) {
-      pads.push(<Pad x={x} y={y} />)
-    }
+const SliderComponent = () => {
+  const buttons = [11, 21, 31, 41, 51, 61, 71, 81]
 
-    rows.push(pads)
-  }
-
-  return rows
+  return (
+    <></>
+  )
 }
 
-const SliderButtonMapping = {
-  buttons: [11, 21, 31, 41, 51, 61, 71, 81]
-}
+const Context = createContext({
+  components: [SliderComponent]
+})
 
-const GridContext = createContext('Components')
+const Launchpad = () => {
 
-type Props = { component: any }
-const Launchpad = ({ component }: Props) => {
   // @ts-ignore
   window.api.send(CHANNELS.LP.CLEAR)
 
   return (
-    <>
-      <Slider buttonMap={SliderButtonMapping.buttons}/>
-      {createGrid().map((row) => (
-        <Grid container>
-          {row.map((pad) => (
-            <Grid item>{pad}</Grid>
-          ))}
-        </Grid>
-      ))}
-      <Test />
-    </>
+    <></>
   )
 }
-
-export default Launchpad

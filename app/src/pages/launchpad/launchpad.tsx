77b546378fdc9a1842885c579a32f0c82069887f
Snuffish <snuffish90@gmail.com>
2023-10-11 21:42:24 +0200

    wip

--- app/src/pages/launchpad/launchpad.tsx
+++ app/src/pages/launchpad/launchpad.tsx
@@ -1,25 +1,8 @@
-import { Box, Container } from '@material-ui/core'
 import Debug from 'Components/Debug'
-import Pad from 'Components/Pad'
 import SliderComponent from 'Components/SliderComponent'
 import { CHANNELS } from 'Constants/ipc'
 import { getGridRow } from 'Utils/grid'
 import React from 'react'
-
-const createGrid = () => {
-  const rows = []
-  for (let y = 8; y >= 1; y--) {
-    const pads = []
-    for (let x = 1; x <= 8; x++) {
-      pads.push(<Pad x={x} y={y} />)
-    }
-
-    rows.push(pads)
-  }
-
-  return rows
-}
-
 
 type Props = { component: any }
 const Launchpad = ({ component }: Props) => {

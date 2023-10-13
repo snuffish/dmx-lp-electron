d150a41c0a0e625de4dd940668324c281c27a3b8
Snuffish <snuffish90@gmail.com>
2023-10-13 17:03:36 +0200

    wip

--- app/src/config/snuffish.tsx
+++ app/src/config/snuffish.tsx
@@ -17,15 +17,14 @@ const snuffishConfig: Config = {
   scenes: {
     ledbar: {
       components: [
-        <SliderComponent key='slider1' orientation='horizontal' row={1} sector={1} color={COLORS.RED} />,
+        <SliderComponent key='slider1' row={1} sector={1} color={COLORS.RED} />,
         <SliderComponent key='slider2' row={2} sector={2} color={COLORS.GREEN} />,
         <SliderComponent key='slider3' row={3} sector={3} color={COLORS.BLUE} />,
         <SliderComponent key='slider4' row={4} sector={4} color={COLORS.RED} />,
-        <SliderComponent key='slider5' row={5} sector={5} color={COLORS.GREEN} />,
+        <SliderComponent key='slider5' orientation='horizontal' row={5} sector={5} color={COLORS.GREEN} />,
         <SliderComponent key='slider6' row={6} sector={6} color={COLORS.BLUE} />,
         <SliderComponent key='slider7' row={7} sector={7} color={COLORS.RED} />,
-        <SliderComponent key='slider8' row={8} sector={8} color={COLORS.GREEN} />,
-        <SliderComponent key='slider9' orientation='horizontal' row={5} sector={8} color={COLORS.GREEN} />
+        <SliderComponent key='slider8' orientation='vertical' row={8} sector={8} color={COLORS.GREEN} />,
       ]
     },
     // laser: {
